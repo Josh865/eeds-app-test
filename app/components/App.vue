581b24e1cb9c2dfcfeb651710b4c870d96241699
Josh <joshdwhite@gmail.com>
2019-08-18 15:10:05 -0400

    First commit

---
+++ app/components/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <Page>
+    <ActionBar title="Welcome to NativeScript-Vue!" />
+    <GridLayout columns="*" rows="*">
+      <Button class="message" :text="msg" col="0" row="0" @tap="goToPage" />
+    </GridLayout>
+  </Page>
+</template>
+
+<script>
+import SelectLogInType from './SelectLogInType';
+
+export default {
+  components: {
+    SelectLogInType,
+  },
+
+  data() {
+    return {
+      msg: 'Hello World!',
+    };
+  },
+
+  methods: {
+    goToPage() {
+      this.$showModal(SelectLogInType);
+    },
+  },
+};
+</script>
+
+<style scoped>
+ActionBar {
+  background-color: #53ba82;
+  color: #ffffff;
+}
+
+.message {
+  vertical-align: center;
+  text-align: center;
+  font-size: 20;
+  color: #333333;
+}
+</style>
---
+++ app/components/SelectLogInType.vue
@@ -0,0 +1,13 @@
+<template>
+  <Page>
+    <FlexboxLayout justifyContent="center" alignItems="center">
+      <Label text="How would you like to log in?" />
+    </FlexboxLayout>
+  </Page>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
