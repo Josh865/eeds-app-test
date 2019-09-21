6d1e6f249203a6a90130991a66d09730c29f61cb
Josh <joshdwhite@gmail.com>
2019-09-21 11:51:59 -0400

    Implement PickerField for degree and specialty

--- app/components/CreateAccount.vue
+++ app/components/CreateAccount.vue
@@ -11,89 +11,62 @@
     <ScrollView>
       <CreateAccountCompleted v-if="accountAwaitingApproval" />
 
-      <GridLayout v-else :class="{ 'modal-open': showModal }">
+      <GridLayout v-else>
         <GridLayout class="content">
           <StackLayout width="90%" marginTop="20">
             <StackLayout marginBottom="20">
-              <Label text="First Name" marginBottom="3" />
-              <TextField
-                v-model="userInfo.first"
-                hint="First Name"
-                class="text-input"
-              />
+              <!-- <Label text="First Name" marginBottom="3" /> -->
+              <TextField v-model="firstName" hint="First Name" />
             </StackLayout>
 
             <StackLayout marginBottom="20">
-              <Label text="Last Name" marginBottom="3" />
-              <TextField
-                v-model="userInfo.last"
-                hint="Last Name"
-                class="text-input"
-              />
+              <!-- <Label text="Last Name" marginBottom="3" /> -->
+              <TextField v-model="lastName" hint="Last Name" />
             </StackLayout>
 
             <StackLayout marginBottom="20">
-              <Label text="Email" marginBottom="3" />
-              <TextField
-                v-model="userInfo.email"
-                hint="Email"
-                class="text-input"
-              />
+              <!-- <Label text="Email" marginBottom="3" /> -->
+              <TextField v-model="email" hint="Email" />
             </StackLayout>
 
             <StackLayout marginBottom="20">
-              <Label text="ZIP" marginBottom="3" />
-              <TextField
-                v-model="userInfo.zip"
-                hint="ZIP Code"
-                class="text-input"
-              />
+              <!-- <Label text="ZIP" marginBottom="3" /> -->
+              <TextField v-model="zip" hint="ZIP Code" />
             </StackLayout>
 
             <PickerField
+              ref="degreePicker"
               for="degree in degrees"
               hint="Degree"
-              pickerTitle="Josh"
+              pickerTitle="Select Your Degree"
               textField="Degree_Name"
               valueField="Degree_ID"
+              marginBottom="20"
+              @textChange="setDegreeId"
             >
               <v-template>
-                <GridLayout columns="auto, *" rows="auto, *">
-                  <Label
-                    :text="degree.Degree_ID"
-                    col="0"
-                    row="1"
-                    marginBottom="20"
-                  />
-                  <Label
-                    :text="degree.Degree_Name"
-                    col="1"
-                    row="0"
-                    rowSpan="2"
-                  />
-                </GridLayout>
+                <Label :text="degree.Degree_Name" padding="15" fontSize="16" />
               </v-template>
             </PickerField>
 
-            <!-- <StackLayout marginBottom="20">
-              <Label text="Degree" marginBottom="3" />
-              <Button
-                :text="selectedDegreeName"
-                androidElevation="0"
-                class="text-input"
-                @tap="openDegreePicker"
-              />
-            </StackLayout> -->
-
-            <StackLayout>
-              <Label text="Specialty" marginBottom="3" />
-              <Button
-                :text="selectedSpecialtyName"
-                androidElevation="0"
-                class="text-input"
-                @tap="openSpecialtyPicker"
-              />
-            </StackLayout>
+            <PickerField
+              ref="specialtyPicker"
+              for="specialty in specialties"
+              hint="Specialty"
+              pickerTitle="Select Your Specialty"
+              textField="Specialty_Name"
+              valueField="Specialty_ID"
+              modalAnimated="true"
+              @textChange="setSpecialtyId"
+            >
+              <v-template>
+                <Label
+                  :text="specialty.Specialty_Name"
+                  padding="15"
+                  fontSize="16"
+                />
+              </v-template>
+            </PickerField>
 
             <Button
               text="Create Account"
@@ -104,35 +77,6 @@
             />
           </StackLayout>
         </GridLayout>
-
-        <!-- Modal that displays degree and specialty list pickers -->
-        <AbsoluteLayout
-          v-if="showModal"
-          class="modal-wrapper"
-          @tap="closeModal"
-        >
-          <FlexboxLayout width="100%" justifyContent="center">
-            <StackLayout v-if="showDegreePicker" class="modal">
-              <Label text="Select Your Degree" />
-              <ListPicker
-                :items="degreeNames"
-                :selectedIndex="selectedDegreeIndex"
-                @selectedIndexChange="updateSelectedDegreeIndex($event.value)"
-              />
-            </StackLayout>
-
-            <StackLayout v-if="showSpecialtyPicker" class="modal">
-              <Label text="Select Your Specialty" />
-              <ListPicker
-                :items="specialtyNames"
-                :selectedIndex="selectedSpecialtyIndex"
-                @selectedIndexChange="
-                  updateSelectedSpecialtyIndex($event.value)
-                "
-              />
-            </StackLayout>
-          </FlexboxLayout>
-        </AbsoluteLayout>
       </GridLayout>
     </ScrollView>
   </Page>
@@ -151,25 +95,14 @@ export default {
 
   data() {
     return {
-      userInfo: {
-        first: '',
-        last: '',
-        email: '',
-        zip: '',
-      },
+      firstName: '',
+      lastName: '',
+      email: '',
+      zip: '',
+      degreeId: '',
+      specialtyId: '',
       degrees: [],
-      degreeNames: [],
-      selectedDegreeIndex: 0,
-      selectedDegreeName: 'Select Your Degree',
-      selectedDegreeId: null,
       specialties: [],
-      specialtyNames: [],
-      selectedSpecialtyIndex: 0,
-      selectedSpecialtyName: 'Select Your Specialty',
-      selectedSpecialtyId: null,
-      showDegreePicker: false,
-      showSpecialtyPicker: false,
-      showModal: false,
       accountAwaitingApproval: false,
       // accountAwaitingApproval: appSettings.getBoolean(
       //   'accountAwaitingApproval',
@@ -179,14 +112,12 @@ export default {
   },
 
   watch: {
-    selectedDegreeId(newVal, oldVal) {
-      if (newVal != oldVal) {
-        // Get rid of the old values related to the specialty
-        this.resetSpecialityValues();
+    degreeId() {
+      // Clear selected specialty since it may be incompatible with newly selected degree
+      this.$refs.specialtyPicker.nativeView.selectedValue = null;
 
-        // Get the specialties available for the newly selected degree
-        this.fetchSpecialties();
-      }
+      // Get the specialties available for the selected degree
+      this.fetchSpecialties();
     },
   },
 
@@ -200,90 +131,31 @@ export default {
         .getJSON(`https://www.eeds.com/ajax_functions.aspx?Function_ID=142`)
         .then(response => {
           this.degrees = response;
-          this.degreeNames = this.degrees.map(degree => degree.Degree_Name);
         })
         .catch(err => {
           alert('Error fetching degrees.');
         });
     },
 
-    updateSelectedDegreeIndex(newIndex) {
-      this.selectedDegreeIndex = newIndex;
-    },
-
-    updateSelectedSpecialtyIndex(newIndex) {
-      this.selectedSpecialtyIndex = newIndex;
+    setDegreeId() {
+      this.degreeId = this.$refs.degreePicker.nativeView.selectedValue;
     },
 
     fetchSpecialties() {
       httpModule
         .getJSON(
-          `https://www.eeds.com/ajax_functions.aspx?Function_ID=88&Degree_ID=${this.selectedDegreeId}`
+          `https://www.eeds.com/ajax_functions.aspx?Function_ID=88&Degree_ID=${this.degreeId}`
         )
         .then(response => {
           this.specialties = response;
-          this.specialtyNames = this.specialties.map(
-            specialty => specialty.Specialty_Name
-          );
         })
         .catch(err => {
           alert('Error fetching specialties.');
         });
     },
 
-    openDegreePicker() {
-      // Make sure the specialty picker is closed so they don't appear at the same time
-      this.showSpecialtyPicker = false;
-
-      this.showModal = true;
-      this.showDegreePicker = true;
-    },
-
-    openSpecialtyPicker() {
-      // Make sure the degree picker is closed so they don't appear at the same time
-      this.showDegreePicker = false;
-
-      this.showModal = true;
-      this.showSpecialtyPicker = true;
-    },
-
-    closeModal() {
-      this.showModal = false;
-
-      // Stuff to do if they just chose a degree
-      if (this.showDegreePicker) {
-        this.selectedDegreeId = this.degrees[
-          this.selectedDegreeIndex
-        ].Degree_ID;
-
-        this.selectedDegreeName = this.degrees[
-          this.selectedDegreeIndex
-        ].Degree_Name;
-
-        this.showDegreePicker = false;
-      }
-
-      // Stuff to do if they just chose a specialty
-      if (this.showSpecialtyPicker) {
-        this.selectedSpecialtyId = this.specialties[
-          this.selectedSpecialtyIndex
-        ].Specialty_ID;
-
-        this.selectedSpecialtyName = this.specialties[
-          this.selectedSpecialtyIndex
-        ].Specialty_Name;
-
-        this.showSpecialtyPicker = false;
-      }
-    },
-
-    // This prevents any weirdness when the user switches their specialty after making an
-    // initial selection.
-    resetSpecialityValues() {
-      this.selectedSpecialtyIndex = 0;
-      this.specialties = [];
-      this.specialtyNames = [];
-      this.selectedSpecialtyName = 'Select Your Specialty';
+    setSpecialtyId() {
+      this.specialtyId = this.$refs.specialtyPicker.nativeView.selectedValue;
     },
 
     createAccount() {
@@ -326,47 +198,6 @@ export default {
   border-color: lightgray;
   border-radius: 3;
   padding: 10;
-}
-
-@keyframes show {
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-}
-
-.modal-open .content {
-  opacity: 0.2;
-}
-
-.modal-open .modal-wrapper {
-  visibility: visible;
-  animation-name: show;
-  animation-duration: 0.3s;
-  animation-fill-mode: forwards;
-}
-
-.modal-wrapper {
-  visibility: collapse;
-  opacity: 0;
-  background-color: rgba(0, 0, 0, 0.2);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.modal {
-  width: 90%;
-  margin-top: 100;
-  margin-left: auto;
-  margin-right: auto;
-  padding: 10;
-  border-width: 0;
-  border-radius: 5;
-  border-color: black;
-  background-color: white;
 }
 
 .btn {
